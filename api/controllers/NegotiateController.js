d58a764f671c93b86d9b561dceda0e17cf8fbb5c
Cole Howard <uglyboxer@gmail.com>
2017-01-30 17:56:04 -0800

    Negotiation room

---
+++ api/controllers/NegotiateController.js
@@ -0,0 +1,16 @@
+/**
+ * NegotiateController
+ *
+ * @description :: Server-side logic for managing Negotiates
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  	create: function(req, res) {
+      Negotiate.create(req.params.all()).exec(function(err, negotiaion) {
+        if (err) return res.negotiate(err);
+        res.send(200);
+      })
+    }
+};
+
---
+++ api/models/Negotiate.js
@@ -0,0 +1,22 @@
+/**
+ * Negotiate.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  autosubscribe: ['destroy', 'update', 'add:users', 'remove:users'],
+  attributes: {
+    black_player: {
+      collection: 'user',
+  },
+    white_player: {
+      collection: 'user',
+  },
+    board_size: 'integer',
+    komi: 'integer',
+    time_system: 'integer',
+    time_limit: 'string',
+};
+
